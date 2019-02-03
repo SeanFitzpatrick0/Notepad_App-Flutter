71976462b160fa32ffe7c9d7a4b7452dc527b4ea
Sean Fitzpatrick <seanlkfitzpatrick@gmail.com>
2019-02-03 15:46:11 +0000

    Adding notepad.dart file

---
+++ notepad/lib/notepad.dart
@@ -0,0 +1,8 @@
+/*
+Author: Sean Fitzpatrick  2019-2-3
+Description: This is the home screen for the app.
+  Ths contains the list of users notes and dispays them
+  as a list to the user.
+  If no notes exist then will display text to encorage 
+  the usere to add a note.
+*/
