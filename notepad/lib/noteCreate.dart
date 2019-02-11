ae28a5f4ad34f76cc201b8dea4c70239804b7bb6
Sean Fitpatrick <seanlkfitzpatrick@gmail.com>
2019-02-11 17:35:36 +0000

    Adding create note form feilds

--- notepad/lib/noteCreate.dart
+++ notepad/lib/noteCreate.dart
@@ -9,12 +9,126 @@ Description: This is the page used to create new and edit existing
 
 import 'package:flutter/material.dart';
 
-class NoteCreate extends StatelessWidget {
+class NoteCreate extends StatefulWidget {
+  @override
+  _NoteCreateState createState() => _NoteCreateState();
+}
+
+class _NoteCreateState extends State<NoteCreate> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Create Note'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          child: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  _titleBox(),
+                  _colorDropDown(),
+                  _contentBox(),
+                ],
+              ),
+              _isImportantButton(),
+              _submitButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _titleBox() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(Icons.create),
+        labelText: 'Title',
+      ),
+    );
+  }
+
+  Widget _colorDropDown() {
+    String _color;
+    List<String> _colors = ['RED', 'GREEN', 'BLUE'];
+    return FormField(
+      builder: (FormFieldState state) {
+        return InputDecorator(
+          decoration: InputDecoration(
+            icon: const Icon(Icons.color_lens),
+            labelText: 'Color',
+          ),
+          isEmpty: _color == '',
+          child: new DropdownButtonHideUnderline(
+            child: new DropdownButton(
+              value: _color,
+              isDense: true,
+              onChanged: (String newValue) {
+                setState(() {
+                  _color = newValue;
+                  state.didChange(newValue);
+                });
+              },
+              items: _colors.map((String value) {
+                return new DropdownMenuItem(
+                  value: value,
+                  child: new Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _contentBox() {
+    return Expanded(
+      child: TextField(
+        keyboardType: TextInputType.multiline,
+        maxLines: null,
+        decoration: InputDecoration(
+          labelText: 'Content',
+          icon: Icon(Icons.dehaze),
+        ),
+      ),
+    );
+  }
+
+  Widget _isImportantButton() {
+    return Positioned(
+      right: 30,
+      bottom: 90,
+        child: IconButton(
+      tooltip: 'Important',
+      icon: Icon(
+        Icons.star,
+        color: Colors.grey,
+        size: 50.0,
+      ),
+      onPressed: () {
+        print('isImportant Pressed');
+      },
+    ));
+  }
+
+  Widget _submitButton() {
+    return Positioned(
+      bottom: 15.0,
+      right: 15.0,
+      child: FloatingActionButton(
+        tooltip: 'Submit',
+        backgroundColor: Colors.greenAccent,
+        child: Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+        onPressed: () {
+          print('Submit button pressed!');
+        },
       ),
     );
   }
